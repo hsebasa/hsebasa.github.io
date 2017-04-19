3c332c30fa4fa8583e7915a113c578132a795b91
mathdecision <hsebasa@gmail.com>
2017-04-19 18:55:22 -0500

    Se inicializa la página web

--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,11 @@ module.exports = function(config) {
     basePath: './app',
 
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'app/source/components/**/*.js',
-      'view*/**/*.js'
+      'bower_components/angular/angular.utils',
+      'bower_components/angular-route/angular-route.utils',
+      'bower_components/angular-mocks/angular-mocks.utils',
+      'app/source/components/**/*.utils',
+      'view*/**/*.utils'
     ],
 
     autoWatch: true,
