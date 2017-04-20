ce2d9d4d37da0f1915b8e27d6dde420fc21b7bfb
mathdecision <hsebasa@gmail.com>
2017-04-19 20:11:10 -0500

    Se crea directiva que pretende presentar los resultados

--- app/source/directives/infoBox/infoBox.js
+++ app/source/directives/infoBox/infoBox.js
@@ -3,7 +3,13 @@
 angular.module('musicApp.directives')
 
 .directive('musicInfoBox', [function(){
+    function link(scope, element){
+    }
     return {
+        link: link,
+        scope: {
+            information: '='   // Information about the single or the artist or the album to model.
+        },
         templateUrl: 'directives/templates/infoBoxTemplate.html'
     }
 }]
