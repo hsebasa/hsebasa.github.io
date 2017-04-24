44fe7954774d0e593cb5cb757ea9145d4dd2e85a
mathdecision <hsebasa@gmail.com>
2017-04-23 19:27:34 -0500

    Se mejora el crop de imágen

--- app/source/directives/imageCrop/imageCrop.js
+++ app/source/directives/imageCrop/imageCrop.js
@@ -17,6 +17,21 @@ angular.module('musicApp.directives')
                 $scope.isDefaultImage = true;
                 $scope.image2Draw = $scope.defaultImage;
             }
+
+            $scope.isWiderImage = parseFloat($scope.image2Draw.width)/parseFloat($scope.image2Draw.height) > parseFloat($scope.width)/parseFloat($scope.height);
+            $scope.isHigherImage = (parseFloat($scope.image2Draw.width)/parseFloat($scope.image2Draw.height) < parseFloat($scope.width)/parseFloat($scope.height));
+            $scope.sameRatioImage = (!$scope.isWiderImage) && (!$scope.isHigherImage);
+
+            if ($scope.isWiderImage){
+                $scope.marginLeft = ((parseFloat($scope.width)-parseFloat($scope.image2Draw.width)*parseFloat($scope.height)/parseFloat($scope.image2Draw.height))/2) + 'px';
+                $scope.marginTop = 0;
+            } else if ($scope.isHigherImage) {
+                $scope.marginLeft = 0;
+                $scope.marginTop = (2/5)*((parseFloat($scope.height)-parseFloat($scope.image2Draw.height)*parseFloat($scope.width)/parseFloat($scope.image2Draw.width))/2) + 'px';
+            } else {
+                $scope.marginLeft = 0;
+                $scope.marginTop = 0;
+            }
         },
         templateUrl: 'templates/imageAdjust.html'
     }
